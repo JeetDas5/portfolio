dc96a2c50ce35225fe3c5c1cf65e750c1d96b943
JeetDas5 <jeet15083011@gmail.com>
2025-02-04 00:24:48 +0530

    footer format

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,6 @@ import dynamic from "next/dynamic";
 const Footer = () => {
   return (
     <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -37,7 +36,12 @@ const Footer = () => {
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 border rounded-lg border-black-300"
             >
               <Link href={profile.link}>
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+                <img
+                  src={profile.img}
+                  alt={profile.id}
+                  width={20}
+                  height={20}
+                />
               </Link>
             </div>
           ))}
