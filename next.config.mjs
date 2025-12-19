a8f33b2bbe7a473afc464c567401fdd6603fc165
JeetDas5 <jeet15083011@gmail.com>
2025-12-19 19:38:39 +0530

    build: patch render and next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    unoptimized: true,
   },
-  images: { unoptimized: true }
 };
 
 export default nextConfig;
